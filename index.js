b746d29bb75b3c366fa89a9c4526055a91c396b1
Your Name <you@example.com>
2021-12-23 22:02:42 -0800

    init

--- index.js
+++ index.js
@@ -8,29 +8,29 @@ const totalSupplyABI = {
     stateMutability: "view",
     type: "function",
 };
-const HECCirculatingSupply = {
+const CirculatingSupply = {
     inputs: [],
-    name: "HECCirculatingSupply",
+    name: "CirculatingSupply",
     outputs: [{ internalType: "uint256",  name: "",  type: "uint256" }],
     stateMutability: "view",
     type: "function",
 };
 
 const port = 3000
-const ftmRpc = "https://rpc.ftm.tools"
-const hecERC20 = '0x5C4FDfc5233f935f20D2aDbA572F770c2E377Ab0'
-const circSupply = '0x5a0325d0830f10044D82044fd04223F2E0Ea5047'
+const bscRPC = "https://bsc-dataseed.binance.org/"
+const zdERC20 = '0x98051143830fa99848E7059E97AcB03B3cc62403'
+const circSupply = '0x260D8CfB7EFe867245e18bbD20068c67c16203c9'
 
-const provider = new ethers.providers.JsonRpcProvider(ftmRpc);
-const hecContract = new ethers.Contract(hecERC20, [totalSupplyABI], provider);
-const circSupplyContract = new ethers.Contract(circSupply, [HECCirculatingSupply], provider);
+const provider = new ethers.providers.JsonRpcProvider(bscRPC);
+const zdContract = new ethers.Contract(zdERC20, [totalSupplyABI], provider);
+const circSupplyContract = new ethers.Contract(circSupply, [CirculatingSupply], provider);
 
 let totalSupply = 0
 let circulatingSupply = 0
 
 const fetchSupply = async () => {
-    totalSupply = (await hecContract.totalSupply()) / Math.pow(10, 9)
-    circulatingSupply = (await circSupplyContract.HECCirculatingSupply()) / Math.pow(10, 9)
+    totalSupply = (await zdContract.totalSupply()) / Math.pow(10, 9)
+    circulatingSupply = (await circSupplyContract.CirculatingSupply()) / Math.pow(10, 9)
     console.log(circulatingSupply, '/', totalSupply)
 }
 
